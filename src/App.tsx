5af0890df9a2337d1f35e17af3e738776eff013d
Amaro134 <amarogracie134@gmail.com>
2026-07-09 11:52:11 +0300

    feat: wire up admin routes and auth provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import BookAppointment from "./pages/BookAppointment";
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import BookAppointment from './pages/BookAppointment';
+import AdminLogin from './pages/AdminLogin';
+import AdminDashboard from './pages/AdminDashboard';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/book" element={<BookAppointment />} />
+      <Route path="/admin" element={<AdminLogin />} />
+      <Route
+        path="/admin/dashboard"
+        element={
+          <ProtectedRoute>
+            <AdminDashboard />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
